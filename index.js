a6dcfc61498e8d5caf3592274d231a84278143ca
Alec Santamaria <alec@Alecs-MacBook-Pro.local>
2022-09-02 11:32:15 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0
+    for(let i=0; i< this.items.length; i++) {
+      sum += this.items[i];
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    };
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
